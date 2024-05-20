47cc0618f394201c3607bd15464d91410a9cc12f
Spyros <rigassp@gmail.com>
2024-05-21 01:52:54 +0300

    almost done

---
+++ tests/dataset.py
@@ -0,0 +1,92 @@
+# This function was borrowed as-is from the original pykan implementation
+# to be able to compare the models on an equal footing
+import torch
+import numpy as np
+
+def create_dataset(f, 
+                   n_var=2, 
+                   ranges = [-1,1],
+                   train_num=1000, 
+                   test_num=1000,
+                   normalize_input=False,
+                   normalize_label=False,
+                   device='cpu',
+                   seed=0):
+    '''
+    create dataset
+    
+    Args:
+    -----
+        f : function
+            the symbolic formula used to create the synthetic dataset
+        ranges : list or np.array; shape (2,) or (n_var, 2)
+            the range of input variables. Default: [-1,1].
+        train_num : int
+            the number of training samples. Default: 1000.
+        test_num : int
+            the number of test samples. Default: 1000.
+        normalize_input : bool
+            If True, apply normalization to inputs. Default: False.
+        normalize_label : bool
+            If True, apply normalization to labels. Default: False.
+        device : str
+            device. Default: 'cpu'.
+        seed : int
+            random seed. Default: 0.
+        
+    Returns:
+    --------
+        dataset : dic
+            Train/test inputs/labels are dataset['train_input'], dataset['train_label'],
+                        dataset['test_input'], dataset['test_label']
+         
+    Example
+    -------
+    >>> f = lambda x: torch.exp(torch.sin(torch.pi*x[:,[0]]) + x[:,[1]]**2)
+    >>> dataset = create_dataset(f, n_var=2, train_num=100)
+    >>> dataset['train_input'].shape
+    torch.Size([100, 2])
+    '''
+
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    if len(np.array(ranges).shape) == 1:
+        ranges = np.array(ranges * n_var).reshape(n_var,2)
+    else:
+        ranges = np.array(ranges)
+        
+    train_input = torch.zeros(train_num, n_var)
+    test_input = torch.zeros(test_num, n_var)
+    for i in range(n_var):
+        train_input[:,i] = torch.rand(train_num,)*(ranges[i,1]-ranges[i,0])+ranges[i,0]
+        test_input[:,i] = torch.rand(test_num,)*(ranges[i,1]-ranges[i,0])+ranges[i,0]
+        
+        
+    train_label = f(train_input)
+    test_label = f(test_input)
+        
+        
+    def normalize(data, mean, std):
+            return (data-mean)/std
+            
+    if normalize_input == True:
+        mean_input = torch.mean(train_input, dim=0, keepdim=True)
+        std_input = torch.std(train_input, dim=0, keepdim=True)
+        train_input = normalize(train_input, mean_input, std_input)
+        test_input = normalize(test_input, mean_input, std_input)
+        
+    if normalize_label == True:
+        mean_label = torch.mean(train_label, dim=0, keepdim=True)
+        std_label = torch.std(train_label, dim=0, keepdim=True)
+        train_label = normalize(train_label, mean_label, std_label)
+        test_label = normalize(test_label, mean_label, std_label)
+
+    dataset = {}
+    dataset['train_input'] = train_input.to(device)
+    dataset['test_input'] = test_input.to(device)
+
+    dataset['train_label'] = train_label.to(device)
+    dataset['test_label'] = test_label.to(device)
+
+    return dataset
