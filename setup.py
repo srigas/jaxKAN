5c5f583e854809b9e1980ca8cbe1728b67c0774a
Spyros Rigas <rigassp@gmail.com>
2024-11-28 15:46:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jaxkan',
-    version='0.1.7',
+    version='0.1.8',
     packages=find_packages(),
     install_requires=[
         'scipy==1.13.1',
