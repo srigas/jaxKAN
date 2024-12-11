7b648ec13b0eee23c100f57a2806c54f0883300f
srigas <rigassp@gmail.com>
2024-12-11 13:15:34 +0200

    Implemented ChebyKAN and its modified version.

--- jaxkan/layers/__init__.py
+++ jaxkan/layers/__init__.py
@@ -1,10 +1,14 @@
 from .BaseLayer import BaseLayer
 from .SplineLayer import SplineLayer
+from .ChebyLayer import ChebyLayer
+from .ModifiedChebyLayer import ModifiedChebyLayer
 
 def get_layer(layer_type: str):
     layer_map = {
         "base": BaseLayer,
-        "spline": SplineLayer
+        "spline": SplineLayer,
+        "cheby": ChebyLayer,
+        "mod-cheby": ModifiedChebyLayer
     }
     
     if layer_type not in layer_map:
