011641dfd252542dd8e313c50486298a70b228c7
srigas <rigassp@gmail.com>
2025-01-04 12:28:26 +0200

    Extended optimizer state interpolation to spline layer

--- jaxkan/utils/general.py
+++ jaxkan/utils/general.py
@@ -5,20 +5,27 @@ import jax.numpy as jnp
 @jax.jit
 def solve_single_lstsq(A_single, B_single):
     """
-    Simulates linalg.lstsq by reformulating the problem AX = B via the normal equations: (A^T A) X = A^T B
-    This is used instead of linalg.lstsq because it's much faster.
+        Simulates linalg.lstsq by reformulating the problem AX = B via the normal equations: (A^T A) X = A^T B
+        This is used instead of linalg.lstsq because it's much faster.
 
-    Args:
-    -----
-        A_single (jnp.array): matrix A of AX = B
-            shape (M, N)
-        B_single (jnp.array): matrix B of AX = B
-            shape (M, K)
+        Args:
+        -----
+            A_single (jnp.array): matrix A of AX = B
+                shape (M, N)
+            B_single (jnp.array): matrix B of AX = B
+                shape (M, K)
+            
+        Returns:
+        --------
+            single_solution (jnp.array): matrix X of AX = B
+                shape (N, K)
+                
+        Example Usage:
+        --------------
+            A = jnp.array([[2.0, 1.0], [1.0, 3.0]])
+            B = jnp.array([[1.0], [2.0]])
         
-    Returns:
-    --------
-        single_solution (jnp.array): matrix X of AX = B
-            shape (N, K)
+            solution = solve_single_lstsq(A, B)
     """
     AtA = jnp.dot(A_single.T, A_single)
     AtB = jnp.dot(A_single.T, B_single)
@@ -30,20 +37,27 @@ def solve_single_lstsq(A_single, B_single):
 @jax.jit
 def solve_full_lstsq(A_full, B_full):
     """
-    Parallelizes the single case, so that the problem can be solved for matrices with dimensions higher than 2.
-    Essentially, solve_single_lstsq and solve_full_lstsq combined are a workaround, because (unlike PyTorch for example), JAX's libraries do not support lstsq for dims > 2.
+        Parallelizes the single case, so that the problem can be solved for matrices with dimensions higher than 2.
+        Essentially, solve_single_lstsq and solve_full_lstsq combined are a workaround, because (unlike PyTorch for example), JAX's libraries do not support lstsq for dims > 2.
 
-    Args:
-    -----
-        A_full (jnp.array): matrix A of AX = B
-            shape (batch, M, N)
-        B_full (jnp.array): matrix B of AX = B
-            shape (batch, M, K)
+        Args:
+        -----
+            A_full (jnp.array): matrix A of AX = B
+                shape (batch, M, N)
+            B_full (jnp.array): matrix B of AX = B
+                shape (batch, M, K)
+            
+        Returns:
+        --------
+            full_solution (jnp.array): matrix X of AX = B
+                shape (batch, N, K)
+                
+        Example Usage:
+        --------------
+            A = jnp.array([[[2.0, 1.0], [1.0, 3.0]], [[1.0, 2.0], [2.0, 1.0]]])
+            B = jnp.array([[[1.0], [2.0]], [[2.0], [3.0]]])
         
-    Returns:
-    --------
-        full_solution (jnp.array): matrix X of AX = B
-            shape (batch, N, K)
+            solution = solve_full_lstsq(A, B)
     """
     # Define the solver for (*, ., .) dimensions
     solve_full = jax.vmap(solve_single_lstsq, in_axes=(0,0))
@@ -54,31 +68,48 @@ def solve_full_lstsq(A_full, B_full):
     
     
 def interpolate_moments(mu_old, nu_old, new_shape):
-    '''
+    """
         Performs a linear interpolation to assign values to the first and second-order moments of gradients
         of the c_i basis functions coefficients after grid extension
         
         Args:
         -----
             mu_old (jnp.array): first-order moments before extension
-                shape (n_in*n_out, num_basis)
+                shape (n_in*n_out, num_basis) or (n_out, n_in, num_basis)
             nu_old (jnp.array): second-order moments before extension
-                shape (n_in*n_out, num_basis)
-            new shape (tuple): (n_in*n_out, new_num_basis)
+                shape (n_in*n_out, num_basis) or (n_out, n_in, num_basis)
+            new shape (tuple): (n_in*n_out, new_num_basis) or (n_out, n_in, new_num_basis)
         
         Returns:
         --------
             mu_new (jnp.array): first-order moments after extension
-                shape (n_in*n_out, new_num_basis)
+                shape (n_in*n_out, new_num_basis) or (n_out, n_in, new_num_basis)
             nu_new (jnp.array): second-order moments after extension
-                shape (n_in*n_out, new_num_basis)
-    '''
+                shape (n_in*n_out, new_num_basis) or (n_out, n_in, new_num_basis)
+                
+        Example Usage:
+        --------------
+            mu_old = jnp.array([[1, 2, 3], [4, 5, 6]])
+            nu_old = jnp.array([[7, 8, 9], [10, 11, 12]])
+            new_shape = (2, 5)
+            
+            mu_new, nu_new = interpolate_moments(mu_old, nu_old, new_shape)
+    """
     old_shape = mu_old.shape
-    size = old_shape[0]
-    old_j = old_shape[1]
-    new_j = new_shape[1]
+    old_j = old_shape[-1] # This is the dimension along which interpolation occurs
+    new_j = new_shape[-1]
     
-    # Create new indices for the second dimension
+    # At this point, the shape will be either (n_in*n_out, num_basis) if the layer type is 'base'
+    # or (n_out, n_in, num_basis) if the layer type is 'spline'
+    # So we need a generic approach to handle these two types without control statements
+    
+    # Flatten all leading dimensions into a single dimension
+    batch_size = jnp.prod(jnp.array(old_shape[:-1]))
+    # Reshape to (n_in*n_out, num_basis)
+    mu_old_2d = mu_old.reshape((batch_size, old_j))
+    nu_old_2d = nu_old.reshape((batch_size, old_j))
+    
+    # Interpolate along the last dimension
     old_indices = jnp.linspace(0, old_j - 1, old_j)
     new_indices = jnp.linspace(0, old_j - 1, new_j)
 
@@ -86,14 +117,19 @@ def interpolate_moments(mu_old, nu_old, new_shape):
     interpolate_fn = lambda old_row: jnp.interp(new_indices, old_indices, old_row)
 
     # Apply the interpolation function to each row using vmap
-    mu_new = jax.vmap(interpolate_fn)(mu_old)
-    nu_new = jax.vmap(interpolate_fn)(nu_old)
+    mu_new_2d = jax.vmap(interpolate_fn)(mu_old_2d)
+    nu_new_2d = jax.vmap(interpolate_fn)(nu_old_2d)
+    
+    # Reshape back to match the original leading dimensions
+    # but with updated last dimension = new_j
+    mu_new = mu_new_2d.reshape((*old_shape[:-1], new_j))
+    nu_new = nu_new_2d.reshape((*old_shape[:-1], new_j))
     
     return mu_new, nu_new
 
 
 def adam_transition(old_state, model_state):
-    '''
+    """
         Performs the state transition for the Adam optimizer with scheduler after grid extension
         Note that the transition happens in-place, i.e. nothing is returned, the optimizer is simply
         transitioned from the old state to the new
@@ -102,7 +138,13 @@ def adam_transition(old_state, model_state):
         -----
             old_state (tuple): collection of adam state and scheduler state before extension
             model_state (dict): model_state dict of KAN model
-    '''
+                
+        Example Usage:
+        --------------
+            old_state = optimizer.opt_state
+            _, model_state = nnx.split(model)
+            adam_transition(old_state, model_state)
+    """
     # Get old state
     adam_mu, adam_nu = old_state[0].mu, old_state[0].nu
 
